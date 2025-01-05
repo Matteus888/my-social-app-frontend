6ec72d7d020ee0bfcf9674cceba73ebb2e730190
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-05 21:40:01 +0100

    Style contactCard & add avatar pic

--- components/ContactCard.jsx
+++ components/ContactCard.jsx
@@ -1,11 +1,11 @@
 import styles from "../styles/contactCard.module.css";
 import Image from "next/image";
 
-export default function ContactCard() {
+export default function ContactCard({ name, image }) {
   return (
     <div className={styles.card}>
-      <Image src="/Surfer.png" alt="Matt Profile Pic" width={30} height={30} />
-      <p>Matt name</p>
+      <Image src={image} alt={`${name} profile pic`} width={40} height={40} />
+      <p className={styles.name}>{name}</p>
     </div>
   );
 }
--- components/ContactsSection.jsx
+++ components/ContactsSection.jsx
@@ -2,15 +2,29 @@ import styles from "../styles/contactsSection.module.css";
 import ContactCard from "./ContactCard";
 
 export default function ContactsSection() {
-  let cards;
-  for (let i = 0; i < 20; i++) {
-    cards = <ContactCard />;
-  }
-
   return (
     <aside className={styles.contacts}>
-      <h2>Contacts</h2>
-      {cards}
+      <h2 className={styles.title}>Contacts</h2>
+      <ContactCard name="Matt" image="/users/man1.png" />
+      <ContactCard name="John" image="/users/man2.png" />
+      <ContactCard name="Jane" image="/users/woman1.png" />
+      <ContactCard name="Billie" image="/users/man3.png" />
+      <ContactCard name="William" image="/users/man4.png" />
+      <ContactCard name="Giraffe Boy" image="/users/giraffe.png" />
+      <ContactCard name="Kate" image="/users/woman2.png" />
+      <ContactCard name="Jean-Eudes" image="/users/man5.png" />
+      <ContactCard name="Koala Man" image="/users/Koala.png" />
+      <ContactCard name="Girl Power" image="/users/woman3.png" />
+      <ContactCard name="Matt" image="/users/man1.png" />
+      <ContactCard name="John" image="/users/man2.png" />
+      <ContactCard name="Jane" image="/users/woman1.png" />
+      <ContactCard name="Billie" image="/users/man3.png" />
+      <ContactCard name="William" image="/users/man4.png" />
+      <ContactCard name="Giraffe Boy" image="/users/giraffe.png" />
+      <ContactCard name="Kate" image="/users/woman2.png" />
+      <ContactCard name="Jean-Eudes" image="/users/man5.png" />
+      <ContactCard name="Koala Man" image="/users/Koala.png" />
+      <ContactCard name="Girl Power" image="/users/woman3.png" />
     </aside>
   );
 }
