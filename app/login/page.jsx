473952d79a0801a951baa7e0b500827b71a8d53e
Matthieu Chatelin <matthieu888@gmail.com>
2025-01-08 17:11:08 +0100

    LoginPage finished

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,38 +2,78 @@
 
 import styles from "../../styles/login.module.css";
 import InfoCard from "@/components/InfoCard";
+import Footer from "@/components/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Login() {
   const [emailValue, setEmailValue] = useState("");
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [passwordValue, setPasswordValue] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
 
-  const handleEmailChange = (e) => {
-    setEmailValue(e.target.value);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const errorMessages = {
+    404: "User doesn't exist",
+    401: "Invalid password",
   };
 
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPasswordValue(e.target.value);
+  const handleError = (status) => {
+    const message = errorMessages[status] || "An unexpected error occurred. Please try again.";
+    setError(true);
+    setErrorMessage(message);
   };
 
   const handleSubmit = async () => {
-    let response = await fetch("http://localhost:3000/users/signin", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ emailValue, passwordValue }),
-    });
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
-    if (response.ok) {
-      let data = await response.json();
-      console.log("connexion réussi", data);
+    if (emailValue.trim() === "") {
+      setError(true);
+      setErrorMessage("Please enter your email");
+      emailRef.current.focus();
+      return;
+    } else if (!regex.test(emailValue)) {
+      setError(true);
+      setErrorMessage("Invalid email address");
+      emailRef.current.focus();
+      return;
+    } else if (passwordValue.trim() === "") {
+      setError(true);
+      setErrorMessage("Please enter your password");
+      passwordRef.current.focus();
+      return;
+    } else {
+      setError(false);
+      setErrorMessage("");
+
+      try {
+        const response = await fetch("http://localhost:3000/users/signin", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ emailValue, passwordValue }),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          if (data.result) {
+            console.log("Signin successful", data);
+            router.push("/");
+          }
+        } else {
+          handleError(response.status);
+        }
+      } catch (err) {
+        console.error("Error during login:", error);
+        setError(true);
+        setErrorMessage("Unable to connect to the server. Please try again later.");
+      }
     }
   };
 
@@ -43,31 +83,45 @@ export default function Login() {
         <h1 className={styles.title}>My Social App</h1>
         <InfoCard content="You must login to continue." />
         <div className={styles.loginContainer}>
-          <p className={styles.loginTitle}>Connect to My Social App</p>
-          <div className={styles.infoAlert}>
-            <p>You must login to continue.</p>
-          </div>
-          <input className={styles.input} type="text" placeholder="Enter your email" value={emailValue} onChange={handleEmailChange} />
-          <div className={styles.inputPwdContainer}>
+          <form onSubmit={async (e) => e.preventDefault()}>
+            <p className={styles.loginTitle}>Connect to My Social App</p>
+            <div className={styles.infoAlert}>
+              <p>You must login to continue.</p>
+            </div>
             <input
-              className={styles.inputPwd}
-              type={passwordVisible ? "text" : "password"}
-              placeholder="Enter your password"
-              value={passwordValue}
-              onChange={handlePasswordChange}
+              className={styles.input}
+              ref={emailRef}
+              type="email"
+              placeholder="Enter your email"
+              value={emailValue}
+              onChange={(e) => setEmailValue(e.target.value)}
             />
-            {passwordValue && (
-              <FontAwesomeIcon
-                icon={passwordVisible ? faEye : faEyeSlash}
-                onClick={togglePasswordVisibility}
-                aria-label={passwordVisible ? "Hide password" : "Show password"}
+            <div className={styles.inputPwdContainer}>
+              <input
+                className={styles.inputPwd}
+                ref={passwordRef}
+                type={passwordVisible ? "text" : "password"}
+                placeholder="Enter your password"
+                value={passwordValue}
+                onChange={(e) => setPasswordValue(e.target.value)}
               />
-            )}
-          </div>
-          <button type="submit" className={styles.btnConnect} onClick={handleSubmit}>
-            Connect
-          </button>
-          <p className={styles.forgottenLink}>Forgotten account details ?</p> {/* Gestion à mettre en place */}
+              {passwordValue.trim() && (
+                <FontAwesomeIcon
+                  icon={passwordVisible ? faEye : faEyeSlash}
+                  onClick={() => setPasswordVisible(!passwordVisible)}
+                  aria-label={passwordVisible ? "Hide password" : "Show password"}
+                />
+              )}
+            </div>
+            <p className={styles.errorText} role="alert">
+              {error ? errorMessage : <span style={{ visibility: "hidden" }}>Invisible</span>}
+            </p>
+            <button type="submit" className={styles.btnConnect} onClick={handleSubmit}>
+              Connect
+            </button>
+          </form>
+          {/* Forget account à mettre en place */}
+          <p className={styles.forgottenLink}>Forget account details ?</p>
           <div className={styles.lineContainer}>
             <div className={styles.line}></div>
             <p className={styles.lineText}>OR</p>
@@ -78,6 +132,7 @@ export default function Login() {
           </Link>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
